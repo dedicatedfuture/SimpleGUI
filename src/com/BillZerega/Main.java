7e675c66d467d8ea0b7bd9189559d524b047d5b0
William Zerega <Bill@Williams-MacBook-Pro-7.local>
2017-10-11 17:45:13 -0400

    making use of multiple event listeners with inner classes

--- src/com/BillZerega/Main.java
+++ src/com/BillZerega/Main.java
@@ -4,12 +4,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-//implements hte interface saying an instance of SimpleGuiB IS-A ActionListener
-//the button will give events only to ActionListener implementation
-public class Main implements ActionListener {
+
+public class Main{
 
     //make a button
-    JButton button;
+    JLabel label;
     JFrame frame;
 
 
@@ -22,33 +21,58 @@ public class Main implements ActionListener {
 
         public void go(){
             frame = new JFrame();
-            //make a button
-            button = new JButton("Change Colors");
 
+            //make a button for the label
+            JButton labelButton = new JButton("Change Label");
             //links button to listening
-            button.addActionListener(this);
+            labelButton.addActionListener(new LabelListener());
 
+            //make a button for the color
+            JButton colorButton = new JButton("Change Circle");
+            //links button to listening
+            colorButton.addActionListener(new ColorListener());
 
             //Program quits as soon as you close the window
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+
+//      different way to add a  listener:      button.addActionListener(this);
+
+            label = new JLabel("I'm a label");
             MyDrawPanel drawPanel = new MyDrawPanel();
 
             //adds the button to the frames content
-            frame.getContentPane().add(BorderLayout.SOUTH, button);
+            frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
             frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+            frame.getContentPane().add(BorderLayout.EAST, labelButton);
+            frame.getContentPane().add(BorderLayout.WEST, label);
 
 
             //give hte frame a size in pixels
-            frame.setSize(300, 300);
+            frame.setSize(500, 500);
 
              /* makes the frame visible */
             frame.setVisible(true);
         }
 
-        //event to happen when button is clicked
-        public void actionPerformed(ActionEvent event){
-            frame.repaint();
+    //implements the interface saying an instance of LabelListener IS-A ActionListener
+//the button will give events only to ActionListener implementation
+        //inner class used to be able to use 2 different event listeners
+        class LabelListener implements ActionListener{
+            public void actionPerformed(ActionEvent event){
+                if (label.getText() == "I'm a label"){
+                    label.setText("Ouch!");
+                }else{
+                    label.setText("I'm a label");
+                }
+
+            }
         }
 
+        class ColorListener implements ActionListener {
+            //event to happen when button is clicked
+            public void actionPerformed(ActionEvent event) {
+                frame.repaint();
+            }
+        }
 }
--- src/com/BillZerega/MyDrawPanel.java
+++ src/com/BillZerega/MyDrawPanel.java
@@ -11,12 +11,15 @@ public class MyDrawPanel extends JPanel {
         //fills background with black as default
         g.fillRect(0,0, this.getWidth(),this.getHeight());
 
+
+        //creating a random generated color
         int red = (int) (Math.random() * 255);
         int green = (int) (Math.random() * 255);
         int blue = (int) (Math.random() * 255);
 
         Color randomColor = new Color(red, green, blue);
 
+        //need to set the color otherwise it will paint black and not show
         g.setColor(randomColor);
         g.fillOval(70, 70, 100, 100);
     }
